6dfcb71998cc227347844a49426bf7a1b1b51ef1
Louis Tilloy <louis_tilloy@berkeley.edu>
2019-05-15 17:20:24 -0700

    try tiny model with leaky relu

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Activation, Conv2D, BatchNormalization, Conv2DTranspose,\
-    UpSampling2D
+    UpSampling2D, LeakyReLU
 
 
 def get_model(resolution, n_classes=None, is_regression=False):
@@ -189,40 +189,40 @@ def get_tiny_model(resolution, n_classes=None, is_regression=False):
         Conv2D(filters=64, kernel_size=5, strides=2, padding="same",
                kernel_initializer='random_uniform',
                input_shape=(resolution, resolution, 1)),
-        Activation('relu'),
+        LeakyReLU(),
         BatchNormalization(),
 
         # conv 2: (128, 128, 64) -> (64, 64, 128)
         Conv2D(filters=128, kernel_size=3, strides=2, padding="same",
                kernel_initializer='random_uniform',),
-        Activation('relu'),
+        LeakyReLU(),
         BatchNormalization(),
 
         # conv 3: (64, 64, 128) -> (32, 32, 256)
         Conv2D(filters=256, kernel_size=3, strides=2, padding="same",
                kernel_initializer='random_uniform',),
-        Activation('relu'),
+        LeakyReLU(),
         BatchNormalization(),
 
         # conv 4: (32, 32, 256) -> (32, 32, 512)
         Conv2D(filters=512, kernel_size=3, padding="same",
                kernel_initializer='random_uniform',),
-        Activation('relu'),
+        LeakyReLU(),
         BatchNormalization(),
 
         # conv8: (32, 32, 512) -> (64, 64, 256)
         Conv2DTranspose(filters=256, kernel_size=4, strides=2, padding="same",
                  kernel_initializer='random_uniform',),
-        Activation('relu'),
+        LeakyReLU(),
 
         # FixMe: this layer is NOT in the article
         # conv9 (64, 64, 256) -> (256, 256, 256)
         Conv2DTranspose(filters=256, kernel_size=4, strides=4, padding="same",
                  kernel_initializer='random_uniform',),
-        Activation('relu'),
+        LeakyReLU(),
 
         # prediction (256, 256, 256) -> (256, 256, n_classes)
-        Conv2DTranspose(filters=n_classes, kernel_size=1, padding="same",
+        Conv2D(filters=n_classes, kernel_size=1, padding="same",
                         kernel_initializer='random_uniform',),
         Activation('softmax')
     ])
